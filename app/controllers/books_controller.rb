087d24b20986f4c6f06525bdfa33d45b086a6562
mina <minami.o2684@gmail.com>
2023-02-23 03:50:28 +0000

    third

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    
   end
   
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
   
   def create
