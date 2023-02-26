89e0d3364e669779ce5fd557877dd55bd65d9cbd
mina <minami.o2684@gmail.com>
2023-02-26 14:34:24 +0000

    validates

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,17 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @user = current_user
+  
   end
   
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    
-    redirect_to book_path(@book.id)
-    
+    if @book.save
+     redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+    else
+     render :new
+    end
   end
   
   def show
@@ -18,17 +20,18 @@ class BooksController < ApplicationController
     @user = current_user
   end
   
-  
-  
-
   def edit
     @book = Book.find(params[:id])
   end
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'You have updated book successfully.'
+    else
+    render :new
+    end
+  
   end
   
   def destroy
